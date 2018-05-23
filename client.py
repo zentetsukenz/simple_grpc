ced7dd775e9a45ee8e8c4f6da83ad08a4a646024
Wiwatta  Mongkhonchit <zentetsukenz@gmail.com>
2018-05-24 02:01:10 +0700

    Simple compose running lb with service

--- client.py
+++ client.py
@@ -7,7 +7,7 @@ import spec_pb2_grpc
 
 
 def run():
-    channel = grpc.insecure_channel('localhost:50051')
+    channel = grpc.insecure_channel('localhost:8080')
     stub = spec_pb2_grpc.HealthCheckStub(channel)
     response = stub.Ping(spec_pb2.PingRequest())
     print("HealthCheck client received: " + response.stat)
