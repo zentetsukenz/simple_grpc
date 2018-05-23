06241a773cd2a537e344915c114fa3d719fcca41
Wiwatta  Mongkhonchit <zentetsukenz@gmail.com>
2018-05-24 00:19:38 +0700

    Implement simple main

---
+++ server/main.py
@@ -0,0 +1,27 @@
+from concurrent import futures
+import time
+
+import grpc
+
+import spec_pb2
+import spec_pb2_grpc
+
+_ONE_DAY_IN_SECONDS = 60 * 60 * 24
+
+class Ping(spec_pb2_grpc.HealthCheckServicer):
+    def Ping(self, request, context):
+        return spec_pb2.PingResponse(stat='Pong!')
+
+def serve():
+    server = grpc.server(futures.ThreadPoolExecutor(max_workers=10))
+    spec_pb2_grpc.add_HealthCheckServicer_to_server(Ping(), server)
+    server.add_insecure_port('[::]:50051')
+    server.start()
+    try:
+        while True:
+            time.sleep(_ONE_DAY_IN_SECONDS)
+    except KeyboardInterrupt:
+        server.stop(0)
+
+if __name__ == '__main__':
+    serve()
