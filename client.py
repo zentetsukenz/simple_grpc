674a2181fb6c30021d6dcec59d87be5aaec4d720
Wiwatta  Mongkhonchit <zentetsukenz@gmail.com>
2018-05-24 10:39:08 +0700

    Secure connectivity

--- client.py
+++ client.py
@@ -7,8 +7,12 @@ import spec_pb2_grpc
 
 
 def run():
-    channel = grpc.insecure_channel('localhost:8080')
+    cert = open('ca.crt', 'rb').read()
+    creds = grpc.ssl_channel_credentials(root_certificates=cert)
+
+    channel = grpc.secure_channel('localhost:8080', creds)
     stub = spec_pb2_grpc.HealthCheckStub(channel)
+
     response = stub.Ping(spec_pb2.PingRequest())
     print("HealthCheck client received: " + response.stat)
 
