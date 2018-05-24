7b1f0000f88594226cb47c37cb6d12c572d99ece
Wiwatta Mongkhonchit <cheab@omise.co>
2018-05-24 14:47:41 +0700

    Client cert verify

--- client.py
+++ client.py
@@ -8,7 +8,9 @@ import spec_pb2_grpc
 
 def run():
     cert = open('ca.crt', 'rb').read()
-    creds = grpc.ssl_channel_credentials(root_certificates=cert)
+    cert_chain = open('client.crt', 'rb').read()
+    key = open('client.key', 'rb').read()
+    creds = grpc.ssl_channel_credentials(root_certificates=cert, private_key=key, certificate_chain=cert_chain)
 
     channel = grpc.secure_channel('localhost:8080', creds)
     stub = spec_pb2_grpc.HealthCheckStub(channel)
